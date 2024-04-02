bff5a79c4c095a9e5f6a5f1354383b62a4e7aeb9
jorgecebria <jorgecebria.dn@gmail.com>
2024-04-02 12:10:03 +0200

    Agregado mensaje de bienvenida en Start. Al inicio del programa.

--- practica1/src/practica1/Start.java
+++ practica1/src/practica1/Start.java
@@ -6,6 +6,8 @@ public class Start {
 	public static void main(String[] args) {
 		// creamos mecanimos para llamar a cualquier metodo fuero de la clase
 		
+		System.out.println("¡Bienvenido a la aplicación de notas!");
+		
 		
 		notas fc= new notas();
 		
